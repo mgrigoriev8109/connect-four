9cedb9dcb84cca949c113255a44c48465d32387d
Mikhail <mikhailgrigoriev92@gmail.com>
2021-11-04 07:23:52 -0400

    Add #play_turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require_relative 'board'
+require_relative 'player'
 class Game 
-  attr_accessor :gameboard, :border_left, :border_right
+  attr_accessor :gameboard, :player_one, :player_two
  
   def initialize
     @gameboard = ['00', '01', '02', '03', '04', '05', '06', '07', '08'].concat(('09'..'41').to_a)
@@ -14,8 +15,24 @@ class Game
     @border_bottom_right = @border_bottom + @border_right
   end
 
+  def create_players
+    puts "Please One, please enter your name."
+    @player_one = Player.new(gets.chomp, ' x')
+    puts "Thank you #{player_one.name}, your symbol will be X"
+    puts "Please Two, please enter your name."
+    @player_two = Player.new(gets.chomp, ' o')
+    puts "Thank you #{player_two.name}, your symbol will be O"
+  end
+
+  def play_turn 
+    puts "#{@player_one.name}, please select the cell to drop your X."
+    selection(gets.chomp.to_i, ' x')
+    puts "#{@player_two.name}, please select the cell to drop your O."
+    selection(gets.chomp.to_i, ' o')
+  end
+
   def selection(cell_index, symbol)
-    if cell_index > 41 || @gameboard[cell_index] == ' x' || @gameboard[cell_index] == ' o'
+    if cell_index > 41 || @gameboard[cell_index] == ' o' || @gameboard[cell_index] == ' x'
       @gameboard[cell_index - 7] = symbol
     else
       cell_index += 7
@@ -23,27 +40,27 @@ class Game
     end
   end
 
-  def count_connected_cells(starting_cell, move_cell_method, border, current_cell = starting_cell, neighbors = [])
-    if @gameboard[current_cell] != ' x'
+  def count_connected_cells(symbol, starting_cell, move_cell_method, border, current_cell = starting_cell, neighbors = [])
+    if @gameboard[current_cell] != symbol
       neighbors
-    elsif @gameboard[current_cell] == ' x' && border.any?(current_cell.to_s)
+    elsif @gameboard[current_cell] == symbol && border.any?(current_cell.to_s)
       neighbors.push(@gameboard[current_cell])
       neighbors
     else
       neighbors.push(@gameboard[current_cell])
-      count_connected_cells(starting_cell, move_cell_method, border, move_cell_method.call(current_cell), neighbors)
+      count_connected_cells(symbol, starting_cell, move_cell_method, border, move_cell_method.call(current_cell), neighbors)
     end
     neighbors.length - 1
   end
 
-  def count_all_connected_cells(current_cell)
-    if (count_connected_cells(current_cell, method(:to_the_top), @border_top) + count_connected_cells(current_cell, method(:to_the_bottom), @border_bottom)) >= 3
+  def count_all_connected_cells(symbol, current_cell)
+    if (count_connected_cells(symbol, current_cell, method(:to_the_top), @border_top) + count_connected_cells(symbol, current_cell, method(:to_the_bottom), @border_bottom)) >= 3
       true
-    elsif (count_connected_cells(current_cell, method(:to_the_left), @border_left) + count_connected_cells(current_cell, method(:to_the_right), @border_right)) >= 3
+    elsif (count_connected_cells(symbol, current_cell, method(:to_the_left), @border_left) + count_connected_cells(symbol, current_cell, method(:to_the_right), @border_right)) >= 3
       true
-    elsif (count_connected_cells(current_cell, method(:to_the_top_left), @border_top_left) + count_connected_cells(current_cell, method(:to_the_bottom_right), @border_bottom_right)) >= 3
+    elsif (count_connected_cells(symbol, current_cell, method(:to_the_top_left), @border_top_left) + count_connected_cells(symbol, current_cell, method(:to_the_bottom_right), @border_bottom_right)) >= 3
       true
-    elsif (count_connected_cells(current_cell, method(:to_the_top_right), @border_top_right) + count_connected_cells(current_cell, method(:to_the_bottom_left), @border_bottom_left)) >= 3
+    elsif (count_connected_cells(symbol, current_cell, method(:to_the_top_right), @border_top_right) + count_connected_cells(symbol, current_cell, method(:to_the_bottom_left), @border_bottom_left)) >= 3
       true
     else
       false
@@ -86,18 +103,8 @@ end
 
 new_game = Game.new
 new_game.gameboard
-
+new_game.create_players
 new_board = Board.new 
 new_board.cells = new_game.gameboard
-puts "selected board"
-new_game.selection(0, ' x')
-new_game.selection(1, ' x')
-new_game.selection(1, ' x')
-new_game.selection(2, ' x')
-new_game.selection(2, ' x')
-new_game.selection(2, ' x')
-new_game.selection(3, ' o')
-new_game.selection(3, ' o')
-new_game.selection(3, ' o')
-new_game.selection(3, ' x')
+new_game.play_turn
 new_board.show
